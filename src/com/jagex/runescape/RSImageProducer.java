1f1bf81bf9c2f5805e51cd89bf14daf55ccf6db3
James Monger <jameskmonger@hotmail.co.uk>
2018-06-21 12:01:34 +0100

    Move disclaimer into readme (#8)
    * Add notice to readme
    
    * Begin removing disclaimer from classes
    
    * Remove disclaimer from more classes
    
    * Remove disclaimer from last classes

--- src/com/jagex/runescape/RSImageProducer.java
+++ src/com/jagex/runescape/RSImageProducer.java
@@ -1,24 +1,5 @@
 package com.jagex.runescape;
 
-/*
- * This file is part of the RuneScape client
- * revision 317, which was publicly released
- * on the 13th of June 2005.
- * 
- * This file has been refactored in order to
- * restore readability to the codebase for
- * educational purposes, primarility to those
- * with an interest in game development.
- * 
- * It may be a criminal offence to run this
- * file. This file is the intellectual property
- * of Jagex Ltd.
- */
-
-/* 
- * This file was renamed as part of the 317refactor project.
- */
-
 import java.awt.*;
 import java.awt.image.*;
 
