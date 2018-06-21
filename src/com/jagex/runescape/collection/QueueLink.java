5079923cc4f38784c27291e67cf904954b5b27e9
James Monger <jkmonger96@gmail.com>
2018-06-21 17:40:41 +0100

    Begin refactoring 'QueueLink'

--- src/com/jagex/runescape/collection/QueueLink.java
+++ src/com/jagex/runescape/collection/QueueLink.java
@@ -3,21 +3,16 @@ package com.jagex.runescape.collection;
 public class QueueLink extends Linkable {
 
 	public QueueLink prevNodeSub;
-
-	QueueLink nextNodeSub;
-
-	public static int anInt1305;
-
-	public QueueLink() {
-	}
+	public QueueLink nextNodeSub;
 
 	public final void unlist() {
 		if (nextNodeSub == null) {
-		} else {
-			nextNodeSub.prevNodeSub = prevNodeSub;
-			prevNodeSub.nextNodeSub = nextNodeSub;
-			prevNodeSub = null;
-			nextNodeSub = null;
+			return;
 		}
+		
+		nextNodeSub.prevNodeSub = prevNodeSub;
+		prevNodeSub.nextNodeSub = nextNodeSub;
+		prevNodeSub = null;
+		nextNodeSub = null;
 	}
 }
